f6efb239e81ed927ad9c47bc3fb4494ef3352a81
brugger <kim.brugger@uib.no>
2022-03-03 15:41:57 +0100

    filter for nirvana parsing, not sure where this is going...

---
+++ bin/nirvana-filter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# Kim Brugger (24.02.2022) kbr(at)brugger.dk
+
+import sys
+import os
+import pprint as pp
+import argparse
+import gzip
+import json
+
+
+import kbr.json_utils as json_utils
+
+import maf.nirvana as nirvana
+
+
+sys.path.append('.')
+
+
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='nirvana-filter: ')
+
+
+    parser.add_argument('-b', '--biotype', help="proteinCoding, ?q")
+    parser.add_argument('-r', '--region', help="region to report variants in")
+    parser.add_argument('-g', '--gnomad-freq', help="max allele frequency in gnomad, -1 == all")
+
+    parser.add_argument('-c', '--clinvar', help="clinvar significance: not provided, uncertain significance, benign, likely benign, likely pathogenic, pathogenic ")
+    parser.add_argument('-v', '--verbose', default=4, action="count",  help="Increase the verbosity of logging output")
+    parser.add_argument('nirvana', nargs='+', help="nirvana output file")
+
+    args = parser.parse_args()
+
+
+    infile = args.nirvana[0]
+
+    filter = {}
+    if args.biotype:
+        filter['bioType'] = args.biotype
+
+    if args.clinvar:
+        filter['inClinvar'] = args.clinvar
+
+    if args.gnomad_freq:
+        filter['allAf'] = float(args.gnomad_freq)
+
+
+    n = nirvana.parse_annotation( infile, filter=filter )
+    pp.pprint(n)
+
+
+if __name__ == "__main__":
+    main()
--- maf/nirvana.py
+++ maf/nirvana.py
@@ -1,36 +1,144 @@
-import json
 import sys
-import gzip
+import re
 import pprint as pp
 
 import kbr.json_utils as json_utils
 
 
-def parse_annotation(infile:str) -> list:
+def _position_info(locus:dict, sample_list:list, filter:dict={}, unknown_as_ref=True) -> dict:
+    
+#    print( filter )
+
+    # if gt == . unknown_as_ref will transform it to 0/0
+
+    res = {}
+
+    samples = len(sample_list)
+    base_var = { 'chrom': locus['chromosome'],
+                 'pos': locus[ 'position' ],
+                 'ref': locus['refAllele']}
+
+#    alts = dict(enumerate(locus['altAlleles']))
+    alts = dict([(value, key + 1) for key, value in enumerate(locus['altAlleles'])])
+#    print( alts )
+
+    gts = {}
+    for index, sample in enumerate(locus['samples']):
+        if 'isEmpty' in sample:
+            continue
+#        print( sample) 
+        if unknown_as_ref and sample['genotype'] in ["./.", ".", ".|."]:
+            sample['genotype'] = "0/0"
+
+        gts[ sample[ 'genotype'] ] = gts.get(sample[ 'genotype'], [])
+        gts[ sample[ 'genotype'] ].append( sample_list[index])
 
 
-    if ".gz" in infile:
-        with gzip.open(infile, 'rt', encoding='UTF-8') as zipfile:
-            annotations = json.load(zipfile)
-    else:
-        annotations = json_utils.read( infile )
+    gts_af = {}
+    for gt in gts:
+        a1, a2 = re.split("/|\|", gt)
+        if a1 == a2:
+            gts_af[ gt ] = 2*len(gts[gt])/(samples * 2)
+        else:
+            gts_af[ gt ] = len(gts[gt])/(samples * 2)
 
-#    print( annotations )
 
-#    for k in annotations.keys():
-#        print( k )
+#    pp.pprint( gts_af )
 
+
+    for variant in locus['variants']:
+
+        print_variant = False
+        if filter == {}:
+            print_variant = True
+
+#        pp.pprint( variant )
+        var = base_var.copy()
+        var['alt'] = variant['altAllele']
+        if len(variant['refAllele']) < 10 and len(variant['altAllele']) < 10:
+            var[ 'vid'] = f"{variant['chromosome']}-{variant['begin']}-{variant['refAllele']}-{variant['altAllele']}"
+        for transcript in variant.get('transcripts', []):
+            for key in transcript:
+                if key in filter and transcript[ key ] in filter[ key ]:
+                    print_variant = True
+                    for k in ['hgnc', 'consequence', 'isCanonical', 'transcript', 'siftPrediction', 'polyPhenPrediction', 'cdsPos', 'aminoAcids']:
+                        if k not in transcript:
+                            continue
+
+                        if isinstance(transcript[k], list):
+                            var[ k ] = ";".join(transcript[k])
+                        else:
+                            var[ k ] = transcript[k]
+                    break
+
+            else:
+                pass
+
+
+        if 'inClinvar' in filter and 'clinvar' in variant:
+            print_variant = False
+
+        for clinvar in variant.get('clinvar', []):
+
+            if 'inClinvar' in filter and filter['inClinvar'] in clinvar['significance']:
+                print_variant = True
+
+            for k in ['significance', 'reviewStatus', 'phenotypes']:
+                if k not in clinvar:
+                    continue
+                if isinstance(clinvar[k], list):
+                    var[ k ] = ";".join(clinvar[k])
+                else:
+                    var[ k ] = clinvar[k]
+
+
+        if 'gnomad' in variant:
+            if 'allAf' in filter and float( filter['allAf']) < variant['gnomad'].get('allAf', 1.0):
+                print_variant = True
+
+        for mitomap in variant.get('mitomap', []):
+            for k in ['diseases', 'status', 'significance']:
+                if k not in mitomap:
+                    continue
+                if isinstance(mitomap[k], list):
+                    var[ k ] = ";".join(mitomap[k])
+                else:
+                    var[ k ] = mitomap[k]
+
+
+        if print_variant:
+            print(var)
+
+    return res
+
+
+
+
+
+
+def parse_annotation(infile:str, filter:dict={}) -> list:
+
+
+    annotations = json_utils.read( infile )
+
+    samples = annotations['header'].get('samples',[])
     filtered = []
 
-    for annotation in annotations['positions']:
-#        pp.pprint( annotation )
-        for alt in annotation['altAlleles']:
-            base_var = { 'chrom': annotation['chromosome'],
-                    'pos': annotation[ 'position' ],
-                    'ref': annotation['refAllele'],
-                    'alt': alt}
 
-            for variant in annotation['variants']:
+    for position in annotations['positions']:
+#        pp.pprint( annotation )
+#        base_var = { 'chrom': position['chromosome'],
+#                     'pos': position[ 'position' ],
+#                     'ref': annotation['refAllele']}
+
+        gts = _position_info( position, samples, filter=filter )
+        continue
+        sys.exit()
+#       alts = annotation['altAlleles']
+
+
+        for variant in annotation['variants']:
+            if 1:
 
                 if 'transcripts' in variant:
                     for transcript in variant['transcripts']:
